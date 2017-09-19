12ae977c3684233769f2e5084c6afc1f8452787c
GrCOTE7 <grcote7@gmail.com>
2017-09-19 17:28:51 +0200

    User plugin overwrite: Add 2 fields: Fb and Bio

--- plugins/grcote7/profile/Plugin.php
+++ plugins/grcote7/profile/Plugin.php
@@ -1,14 +1,44 @@
 <?php namespace Grcote7\Profile;
-
+// 123123
+// $2y$10$06ZVAqSvyWqtEQ6X5VuOFuNnKpLKvSZUlEdzSMYCSS1f1Hl8zCAV.
 use System\Classes\PluginBase;
+use RainLab\User\Models\User as UserModel;
+use RainLab\User\Controllers\Users as UsersController;
 
-class Plugin extends PluginBase
-{
-    public function registerComponents()
-    {
-    }
+class Plugin extends PluginBase {
 
-    public function registerSettings()
-    {
-    }
+  public function registerComponents() {
+  }
+
+
+  public function registerSettings() {
+  }
+
+
+  public function boot() {
+
+    UserModel::extend(function ($model) {
+
+      $model->addFillable([
+                            'facebook',
+                            'bio'
+                          ]);
+    });
+
+    UsersController::extendFormFields(function ($form, $model, $context) {
+
+      $form->addTabFields([
+                            'facebook' => [
+                              'label' => 'Facebook',
+                              'type'  => 'text',
+                              'tab'   => 'Profile'
+                            ],
+                            'bio'      => [
+                              'label' => 'Biography',
+                              'type'  => 'textarea',
+                              'tab'   => 'Profile'
+                            ]
+                          ]);
+    });
+  }
 }
