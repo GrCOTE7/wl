cf5bcdbc3e36740321e1911db3830cc5a915d954
GrCOTE7 <grcote7@gmail.com>
2017-09-27 13:27:09 +0200

    Add seach for dynamic pages (films)

--- plugins/grcote7/movies/Plugin.php
+++ plugins/grcote7/movies/Plugin.php
@@ -26,4 +26,59 @@ class Plugin extends PluginBase {
 
   public function registerSettings() {
   }
+
+
+  public function boot() {
+
+    \Event::listen('offline.sitesearch.query', function ($query) {
+
+      // Search your plugin's contents
+      $items = Models\Movie::where('name', 'like', "%${query}%")
+                           ->orWhere('description', 'like', "%${query}%")
+                           ->get();
+
+      // Now build a results array
+      $results = $items->map(function ($item) use ($query) {
+
+        // If the query is found in the title, set a relevance of 2
+        $relevance = mb_stripos($item->title, $query) !== false ? 2 : 1;
+        if ($item->poster) {
+
+          return [
+            'title'     => $item->name,
+            'text'      => $item->description,
+            'url'       => '/films/film/' . $item->slug,
+            'thumb'     => $item->poster->first(),
+            // Instance of System\Models\File
+            'relevance' => $relevance,
+            // higher relevance results in a higher
+            // position in the results listing
+            // 'meta' => 'data',       // optional, any other information you want
+            // to associate with this result
+            // 'model' => $item,       // optional, pass along the original model
+          ];
+        }
+        else {
+          return [
+            'title'     => $item->name,
+            'text'      => $item->description,
+            'url'       => '/films/film/' . $item->slug,
+            'relevance' => $relevance,
+            // higher relevance results in a higher
+            // position in the results listing
+            // 'meta' => 'data',       // optional, any other information you want
+            // to associate with this result
+            // 'model' => $item,       // optional, pass along the original model
+          ];
+        }
+      });
+
+      return [
+        'provider' => 'Film',
+        // The badge to display for this result
+        'results'  => $results,
+      ];
+    });
+  }
+
 }
