50640eec9649565049d215656560370e39061d42
GrCOTE7 <grcote7@gmail.com>
2017-10-03 06:03:10 +0200

    Insertion de JS

---
+++ themes/olympos/assets/js/app.js
@@ -0,0 +1,13 @@
+function sayHi(name) {
+    $('#maCase').html('<b>Salut, ' + name + ' !</b>');
+}
+
+// sayHi('Toi');
+
+
+var salut = function Salut(name) {
+    $('#maCase').html('<b>Salut, ' + name + ' !</b>');
+};
+
+
+new salut('Tôa');
